a8b3db71db0a298febb73a22ff23bd931528413c
usaidabbasi2008 <gmr29292@gmail.com>
2026-09-17 16:12:34 +0500

    issues

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
-import AIChat from './components/AIchat'
+
 function page() {
   return (
     <div>
